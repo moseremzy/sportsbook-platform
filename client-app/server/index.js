b4058818a115b9fc1b93acc3ebcaa9b167daa371
moses <agbaojemoses@gmail.com>
2026-06-10 16:45:33 +0100

    console.log update

--- client-app/server/index.js
+++ client-app/server/index.js
@@ -1,27 +1,7 @@
 require("dotenv").config();
 
-const express = require("express");
-const port = process.env.PORT || 9000;
-const router = require("./routes/router.js");
-const cors = require("cors");
 const path = require("path");
 const fs = require("fs")
-const session = require("express-session");
-const passport = require("passport");
-const sessionConfig = require("./middlewares/session");
-const { getSettings } = require('./middlewares/settings')
-const RESOURCES_ROOT = path.resolve(__dirname, '../../resources');
-const base_url = process.env.BASE_URL
-const app = express();
-
-
-app.set("trust proxy", 1);
-
-
-require("./cron/cron_job"); // THIS triggers the cron file
-
-
-
 const util = require('util');
 const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags: 'w'});
 const log_stdout = process.stdout;
@@ -35,6 +15,24 @@ console.error = (d, e, f, g) => {
   log_file.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
   log_stdout.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
 }
+
+const express = require("express");
+const port = process.env.PORT || 9000;
+const router = require("./routes/router.js");
+const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const sessionConfig = require("./middlewares/session");
+const { getSettings } = require('./middlewares/settings')
+const RESOURCES_ROOT = path.resolve(__dirname, '../../resources');
+const base_url = process.env.BASE_URL
+const app = express();
+
+
+app.set("trust proxy", 1);
+
+
+require("./cron/cron_job"); // THIS triggers the cron file
 
 
 
