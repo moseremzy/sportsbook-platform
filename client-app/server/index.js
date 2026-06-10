98fa69dc99382f95fa6d853c75c615327d3617fc
moses <agbaojemoses@gmail.com>
2026-06-10 16:18:54 +0100

    latest update

--- client-app/server/index.js
+++ client-app/server/index.js
@@ -22,19 +22,19 @@ require("./cron/cron_job"); // THIS triggers the cron file
 
 
 
-const util = require('util');
-const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags: 'w'});
-const log_stdout = process.stdout;
+// const util = require('util');
+// const log_file = fs.createWriteStream(__dirname + '/debug.log', {flags: 'w'});
+// const log_stdout = process.stdout;
 
-console.log = (d, e, f, g) => {
-  log_file.write(util.format('LOG: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
-  log_stdout.write(util.format('LOG: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
-}
+// console.log = (d, e, f, g) => {
+//   log_file.write(util.format('LOG: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+//   log_stdout.write(util.format('LOG: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+// }
 
-console.error = (d, e, f, g) => {
-  log_file.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
-  log_stdout.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
-}
+// console.error = (d, e, f, g) => {
+//   log_file.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+//   log_stdout.write(util.format('ERROR: ', d?d:'', e?e:'', f?f:'', g?g:'') + '\n');
+// }
 
 
 
